e625222703e2ae560526df7883cd1731b4dda46f
YaronDaya <yarondaya12p@gmail.com>
2019-04-30 17:43:39 +0300

    home page

---
+++ home.php
@@ -0,0 +1,12 @@
+<<!DOCTYPE html>
+<html>
+<header>
+<<style media="screen">
+  .error{margin:0 auto;width:100%;height:100%;text-align:center;font-size:140%;border:1px solid black;}
+</style>
+
+</header>
+<body>
+<div class="error">yaron hatol</div>
+</body>
+</html>
