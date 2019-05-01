5ecb08d96dcc8a642b80b32a043f8b30d50a9353
Adi Cantor <Adi.Cantor@tama.co.il>
2019-05-01 12:41:21 +0300

    a

--- includes/config.php
+++ includes/config.php
@@ -58,7 +58,7 @@ try {
 ///////////////////////////////////////////
 // $db = new PDO($dsn, $user, $password);
 
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS);
 echo 'Connected to Database<br/>';
 
 }
