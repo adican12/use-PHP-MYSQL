234418a520845e81c3bfd4c9121aa9ff69289e39
Adi Cantor <Adi.Cantor@tama.co.il>
2019-05-14 20:03:05 +0300

    ss

--- includes/config.php
+++ includes/config.php
@@ -5,7 +5,7 @@ echo "<br>welcome congif!<br>";
 $servername = "35.237.162.70";
 $username = "root";
 $password = "1";
-$dbname = "catif";
+$dbname = "catifi";
 echo "servername: $servername , user: $username , password: $password , db: $dbname<br>";
 
 // Create connection
