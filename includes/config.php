432675d484e0a9adb2f5469045a48a6f76ae9f5e
Adi Cantor <Adi.Cantor@tama.co.il>
2019-05-14 19:35:15 +0300

    a

--- includes/config.php
+++ includes/config.php
@@ -6,10 +6,10 @@ $dsn = "35.237.162.70";
 $user = "root";
 $password = "1";
 $dbname = "catif";
-echo "dns: ".$dsn." , user: ".$user." , password: ".$password." ,db:".$dbname;
+echo "dns: ".$dsn." , user: ".$user." , password: ".$password." ,db:".$dbname."<br>";
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($dsn, $user, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
