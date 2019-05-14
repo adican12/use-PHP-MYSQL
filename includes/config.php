f9a1722cbdc38dc77c0c6f81e323efb74ee4a10e
Adi Cantor <Adi.Cantor@tama.co.il>
2019-05-14 21:57:54 +0300

    as

--- includes/config.php
+++ includes/config.php
@@ -1,40 +1,26 @@
 ﻿<?php
 
-$link = mysqli_connect("35.237.162.70", "root", 1, "catifi");
+echo "<br>welcome congif!<br>";
 
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+$servername = "35.231.143.183";
+$username = "root";
+$password = "1";
+$dbname = "catifi";
+echo "servername: $servername , user: $username , password: $password , db: $dbname<br>";
+
+// try{
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+echo "pass conn<br>";
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
-
-// echo "<br>welcome congif!<br>";
+// }catch{
+//   echo "not working";
 //
-// $servername = "localhost";
-// $username = "root";
-// $password = "1";
-// $dbname = "catifi";
-// echo "servername: $servername , user: $username , password: $password , db: $dbname<br>";
-//
-// // try{
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check connection
-// echo "pass conn<br>";
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
 // }
-// // }catch{
-// //   echo "not working";
-// //
-// // }
-// echo 'Connected to Database<br/>';
+echo 'Connected to Database<br/>';
 
 
 ?>
