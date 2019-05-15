d4cab0371481c3ae78b7c52e139f53f484e7803d
Adi Cantor <Adi.Cantor@tama.co.il>
2019-05-15 12:43:30 +0300

    b

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,23 +1,8 @@
 ﻿<?php
-// DB credentials.
-// define('DB_HOST','fdb15.eohost.com');
-// define('DB_USER','2520448_armentum');
-// define('DB_PASS','963852asd');
-// define('DB_NAME','2520448_armentum');
-// // Establish database connection.
-// try
-// {
-// $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-// }
-// catch (PDOException $e)
-// {
-// exit("Error: " . $e->getMessage());
-// }
-
 $servername = "localhost";
 $username = "root";
-$password = "1234catwifi";
-$dbname = "adbasewifi";
+$password = "1";
+$dbname = "catifi";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
