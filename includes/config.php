366fc2912d1a416921dc1b3f1665fb1ee983660c
YaronDaya <yarondaya12p@gmail.com>
2019-05-15 16:56:40 +0300

    no echo

--- includes/config.php
+++ includes/config.php
@@ -12,7 +12,7 @@ $dbname = "catifi";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // // Check connection
-echo "pass conn<br>";
+//echo "pass conn<br>";
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -20,7 +20,7 @@ if ($conn->connect_error) {
 //   echo "not working";
 //
 // }
-echo 'Connected to Database<br/>';
+// echo 'Connected to Database<br/>';
 
 
 ?>
