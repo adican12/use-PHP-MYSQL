574c8440da3a56eb0263250a24ed781fe5cf709e
YaronDaya <yarondaya12p@gmail.com>
2019-05-18 01:06:50 +0300

    change

--- business_user/deleteduser.php
+++ business_user/deleteduser.php
@@ -88,6 +88,24 @@ else{
 						<p>This first section defines your company and its products or services then shows how the benefits you provide set you apart from your competition. It's called a “situation analysis.”
 								Target audiences have become extremely specialized and segmented. No matter your industry, from restaurants to professional services to retail clothing stores, positioning your product or service competitively requires an understanding of your niche market. Not only do you need to be able to describe what you market, but you must also have a clear understanding of what your competitors are offering and be able to show how your product or service provides a better value.</p>
 				</div>
+				<div class="marketing">
+						<h3>Step 2: Define who your target audience is.</h3>
+						<p>Developing a simple, one-paragraph profile of your prospective customer is your next step. You can describe prospects in terms of demographics -- age, sex, family composition, earnings and geographic location -- as well as lifestyle. Ask yourself the following: Are my customers conservative or innovative? Leaders or followers? Timid or aggressive? Traditional or modern? Introverted or extroverted? How often do they purchase what I offer? In what quantity?</p>
+						<br><p><span> WE can Help With this!</span></p></br>
+				</div>
+				<div class="marketing">
+						<h3>Step 3: Make a list of your marketing goals.</h3>
+						<p>What do you want your marketing plan to achieve? For example, are you hoping for a 20 percent increase in sales of your product per quarter? Write down a short list of goals -- and make them measurable so that you’ll know when you’ve achieved them.</p>
+						<p>You need to divide your goals into a few
+For short and long term
+Short-term: Short-term goals can be achieved in a period of weeks to a year
+Long-term goals can be five, 10 or even 20 years; They should be bigger than short-term goals, but still need to be reality</p>
+				</div>
+				<div class="marketing">
+						<h3>Step 5: Set your marketing budget</h3>
+						<p>You’ll need to devote a percentage of projected gross sales to your annual marketing budget. Of course, when starting a business, this may mean using newly acquired funding, borrowing or self-financing. Just bear this in mind -- marketing is absolutely essential to the success of your business. And with so many different kinds of tactics available for reaching out to every conceivable audience niche, there’s a mix to fit even the tightest budget.</p>
+
+				</div>
 							</div>
 						</div>
 					</div>
