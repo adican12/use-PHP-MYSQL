07ce5840cf8cd239b8bd77ae8328a1a33e59d905
YaronDaya <yarondaya12p@gmail.com>
2019-05-19 18:12:34 +0300

    test 3

--- advertiser/notification.php
+++ advertiser/notification.php
@@ -100,17 +100,38 @@ if(isset($_POST['submit']))
 									<div class="panel-heading"> Creat campaign </div>
 									   <div class="panel-body">
 <?php
-$reciver = 'Admin';
+if(isset($_POST['sumbit'])) {
+	$campaignName = $_POST['campaignName'];
+	$budget = $_POST['budget'];
+	$gender = $_POST['gender'];
+	$stratDate = $_POST['stratDate'];
+	$endDate = $_POST['endDate'];
+	$category =$_POST['category'];
+  echo "Campagin Name:".$campaignName."<br>"."the budget is : ".$budget."<br>"."the gender is: ".$gender."<br>";
+  echo "the strat date of the campaign is : ".$stratDate."<br>"."the end date of the campaign is : ".$endDate."<br>";
+  echo "the category is : " .$category;
 
-
-$sql = "SELECT * from  notification where notireciver = '$reciver' order by time DESC";
-$result = $conn->query($sql);
-if($result === false)
-{
-	 user_error("Query failed: ".$conn->error."<br />$sql");
-	 echo "false";
-	 echo "here the false2";
+	$sql = "INSERT INTO `campaign`(`campaignName`,`budget`,`gender`,`stratingDate`,`endDate`,`category`)
+	VALUES('$campaignName','$budget','$gender','$stratDate','$endDate','$category');";
+	if ($conn->query($sql) === TRUE) {
+		echo "<script type='text/javascript'>alert('Insert  Sucessfull!');</script>";
+		echo "<script type='text/javascript'> document.location = 'notification.php'; </script>";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error;
+		echo "<script type='text/javascript'>alert('ERROR   INSTERT!');</script>";
+		$error="Something went wrong. Please try again";
+	}
 }
+
+
+// $sql = "SELECT * from  notification where notireciver = '$reciver' order by time DESC";
+// $result = $conn->query($sql);
+// if($result === false)
+// {
+// 	 user_error("Query failed: ".$conn->error."<br />$sql");
+// 	 echo "false";
+// 	 echo "here the false2";
+// }
 
 
 // $sql = "SELECT * from  notification where notireciver = (:reciver) order by time DESC";
