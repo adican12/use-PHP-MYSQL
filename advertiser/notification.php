376bb7ed2a853b4d8e9c36ad63eda18581274aa9
YaronDaya <yarondaya12p@gmail.com>
2019-05-20 17:55:49 +0300

    try

--- advertiser/notification.php
+++ advertiser/notification.php
@@ -187,14 +187,7 @@ if ($result = $conn->query($sql) === TRUE) {
 		echo "<script type='text/javascript'>alert('ERROR   INSTERT!');</script>";
   }
 
-  if ($result->num_rows > 0) {
-   // output data of each row
-   while($row = $result->fetch_assoc()) {
-       //echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-   }
-} else {
-   echo "0 results";
-}
+$row = $result->fetch_assoc()) ;
 
 $cnt=1;
 //
