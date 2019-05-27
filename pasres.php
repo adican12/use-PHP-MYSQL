83a85efbe3d001564e009e97be7dccd6cbe73d02
YaronDaya <yarondaya12p@gmail.com>
2019-05-27 17:29:09 +0300

    try this.

--- pasres.php
+++ pasres.php
@@ -5,11 +5,12 @@ if (($handle = fopen("business.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         $catgory =explode(","$data);
+        echo $catgory."<br>";
         echo "<p> $num fields in line $row: <br /></p>\n";
         $row++;
         for ($c=0; $c < $num; $c++) {
             // $catgory =explode(","$data);
-            echo $catgory[$c] . "<br />\n";
+          // echo $catgory[$c] . "<br />\n";
             echo $data[$c] . "<br />\n";
         }
     }
