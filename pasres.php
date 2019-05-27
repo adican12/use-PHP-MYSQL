37c1bea19514eb494e8554b6c34d967fe66f73c6
YaronDaya <yarondaya12p@gmail.com>
2019-05-27 18:21:50 +0300

    INSERT TO DB

--- pasres.php
+++ pasres.php
@@ -25,11 +25,11 @@ if (($handle = fopen("business.csv", "r")) !== FALSE) {
         echo $bus_long."<br>";
         $sql = "INSERT INTO `business` ( `category`, `couponID`, `business_name`, `bus_latitude`, `bus_longitude`) VALUES
         ('$catgory','$copunID', '$bus_name','$bus_lat', '$bus_long');";
-        if($conn->query($sql) === false) {
-          user_error("Query failed: ".$conn->error."<br />$sql");
-          echo "false";
+        if($conn->query($sql) === TURE) {
+          echo "<script type='text/javascript'>alert('INSERT INTO feedback Sucessfull!');</script>";
         } else {
-          	echo "<script type='text/javascript'>alert('INSERT INTO feedback Sucessfull!');</script>";
+            user_error("Query failed: ".$conn->error."<br />$sql");
+            echo "false";
         }
         // echo $data[1]."<br>";
         // echo $data[2]."<br>";
