020d3a20c37cba3df9183a0019d3897eaf1b0bfa
YaronDaya <yarondaya12p@gmail.com>
2019-05-27 17:15:11 +0300

    change parser

--- pasres.php
+++ pasres.php
@@ -1,9 +1,11 @@
 <?php
 include('includes/config.php');
+
   $file = file('business.csv');
     foreach($file as $word) {
         $csv[]=explode(',',$word);
     }
     print_r($csv);
 }
+close($file);
 ?>
