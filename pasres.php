97c9bd9c5a8af796900751457dc0a609e4f0b853
YaronDaya <yarondaya12p@gmail.com>
2019-05-28 15:43:38 +0300

    change filename.

--- pasres.php
+++ pasres.php
@@ -2,7 +2,7 @@
 include('includes/config.php');
 
 $row = 1;
-if (($handle = fopen("users.csv", "r")) !== FALSE) {
+if (($handle = fopen("user.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($data);
         echo "<p> $num fields in line $row: <br /></p>\n";
