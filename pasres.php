0427557e1f0f07db42b3dc72ca31f9fc702a5967
YaronDaya <yarondaya12p@gmail.com>
2019-05-28 21:27:06 +0300

    php do not working

--- pasres.php
+++ pasres.php
@@ -1,45 +1,46 @@
 
 <?php
-include('includes/config.php');
-echo " befor if";
-if(isset($_POST['submit'])) {
-  if(getimagesize($_FILES['imagefile']['tmp_name']) == false){
-    echo "<br/> Please Select An Image,";
-  } else {
-    //declare variables
-    $image = $_FILES['imagefile']['tmp_name'];
-    $name = $_FILES['imagefile']['name'];
-    $image = base64_decode(file_get_contents(addslashes($image)));
-    //sql query
-    $sql = "INSERT INTO `ad`(`text`,`price`,`header`,`id`,`image`) VALUES ('TEST',1000,'TEST1',1,'$image')";
-    if($conn->query($sql)) {
-      echo " <br/> Image uploaded Successfully";
-    } else {
-      echo " <br/> Image Failed to uploaded";
-    }
-
-  }
-
-} else {
-  # code..
-}
-// retrieve image form database and display it on html webpage
-
-function displayImageFromDatabase() {
-  $sql = "SELECT * FROM ad;";
-  if($res=$conn->query($sql)){
-    while($row = mysqli_fetch_assoc($res)){
-      echo "the text is : ".$row['text']."<br> /";
-      echo "the price is : ".$row['price']."<br> /";
-      echo "the header is : ".$row['header']."<br> /";
-      echo "the id is : ".$row['id']."<br> /";
-      echo '<img height = "250px" width="250px;" src=data:image;base64'.$row['image']."<br> /";
-    }
-
-  }
-}
-displayImageFromDatabase();
+// include('includes/config.php');
+// echo " befor if";
+// if(isset($_POST['submit'])) {
+//   if(getimagesize($_FILES['imagefile']['tmp_name']) == false){
+//     echo "<br/> Please Select An Image,";
+//   } else {
+//     //declare variables
+//     $image = $_FILES['imagefile']['tmp_name'];
+//     $name = $_FILES['imagefile']['name'];
+//     $image = base64_decode(file_get_contents(addslashes($image)));
+//     //sql query
+//     $sql = "INSERT INTO `ad`(`text`,`price`,`header`,`id`,`image`) VALUES ('TEST',1000,'TEST1',1,'$image')";
+//     if($conn->query($sql)) {
+//       echo " <br/> Image uploaded Successfully";
+//     } else {
+//       echo " <br/> Image Failed to uploaded";
+//     }
+//
+//   }
+//
+// } else {
+//   # code..
+// }
+// // retrieve image form database and display it on html webpage
+//
+// function displayImageFromDatabase() {
+//   $sql = "SELECT * FROM ad;";
+//   if($res=$conn->query($sql)){
+//     while($row = mysqli_fetch_assoc($res)){
+//       echo "the text is : ".$row['text']."<br> /";
+//       echo "the price is : ".$row['price']."<br> /";
+//       echo "the header is : ".$row['header']."<br> /";
+//       echo "the id is : ".$row['id']."<br> /";
+//       echo '<img height = "250px" width="250px;" src=data:image;base64'.$row['image']."<br> /";
+//     }
+//
+//   }
+// }
+// displayImageFromDatabase();
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
