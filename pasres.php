795984934a309b3445bad120243f5d32b8be26d0
YaronDaya <yarondaya12p@gmail.com>
2019-05-28 15:50:07 +0300

    echo the rows

--- pasres.php
+++ pasres.php
@@ -7,41 +7,53 @@ if (($handle = fopen("user.csv", "r")) !== FALSE) {
         $num = count($data);
         echo "<p> $num fields in line $row: <br /></p>\n";
         $row++;
-        for ($c=0; $c < $num; $c++) {
+        // for ($c=0; $c < $num; $c++) {
+        //      echo $data[$c] . "<br />\n";
+        //  }
+        echo " befor:"."<br>";
+        #LEST TRY THIS
+        $id = $data[0];
+        $name = $data[1];
+        $email = $data[2];
+        $password = $data[3];
+        $gender = $data[4];
+        $mobile = $data[5];
+        $user_type = $data[6];
+        $image = $data[7];
+        $birthday = $data[8];
+        $status = $data[9];
+        $user_category = $data[10];
 
-            // $catgory =explode(","$data);
-          // echo $catgory[$c] . "<br />\n";
-             echo $data[$c] . "<br />\n";
+        // $at = floatval ($data[1]);
+        // $bus_long = floatval ($data[2]);
 
-         }
-    //     echo " befor:"."<br>";
-    //     #LEST TRY THIS
-    //     $catgory = $data[3];
-    //     $copunID = intval($data[4]);
-    //     $bus_name = $data[0];
-    //     $bus_lat = floatval ($data[1]);
-    //     $bus_long = floatval ($data[2]);
-    //     echo $catgory."<br>";
-    //     echo $copunID."<br>";
-    //     echo $bus_name."<br>";
-    //     echo $bus_lat."<br>";
-    //     echo $bus_long."<br>";
-    //     echo " after : <br>";
-    //     $sql = "INSERT INTO `business` ( `category`, `couponID`, `business_name`, `bus_latitude`,`bus_longitude`)
-    //      VALUES ('$catgory','$copunID', '$bus_name','$bus_lat', '$bus_long');";
-    //      // echo "after the sql querys"."<br>";
-    //     if($conn->query($sql) === TURE) {
-    //       echo "<script type='text/javascript'>alert('INSERT INTO feedback Sucessfull!');</script>";
-    //     } else {
-    //         user_error("Query failed: ".$conn->error."<br />$sql");
-    //         echo "false"."<br>";
-    //     }
-    //     // echo $data[1]."<br>";
-    //     // echo $data[2]."<br>";
-    //     // echo $data[3]."<br>";
-    //     // echo $data[4]."<br>";
-    //
-    // }
+        echo $id."<br>";
+        echo $name."<br>";
+        echo $email."<br>";
+        echo $password."<br>";
+        echo $gender."<br>";
+        echo $mobile."<br>";
+        echo $user_type."<br>";
+        echo $image."<br>";
+        echo $birthday."<br>";
+        echo $status."<br>";
+        echo $user_category."<br>";
+        // echo " after : <br>";
+        // $sql = "INSERT INTO `business` ( `category`, `couponID`, `business_name`, `bus_latitude`,`bus_longitude`)
+        //  VALUES ('$catgory','$copunID', '$bus_name','$bus_lat', '$bus_long');";
+        //  // echo "after the sql querys"."<br>";
+        // if($conn->query($sql) === TURE) {
+        //   echo "<script type='text/javascript'>alert('INSERT INTO feedback Sucessfull!');</script>";
+        // } else {
+        //     user_error("Query failed: ".$conn->error."<br />$sql");
+        //     echo "false"."<br>";
+        // }
+        // echo $data[1]."<br>";
+        // echo $data[2]."<br>";
+        // echo $data[3]."<br>";
+        // echo $data[4]."<br>";
+
+    }
 
 }
   fclose($handle);
