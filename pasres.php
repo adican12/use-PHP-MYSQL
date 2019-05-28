60970ba51e1d7fdacd6bc84e3c2badace7cb7a77
YaronDaya <yarondaya12p@gmail.com>
2019-05-28 15:59:25 +0300

    ok fix the query.

--- pasres.php
+++ pasres.php
@@ -40,7 +40,7 @@ if (($handle = fopen("user.csv", "r")) !== FALSE) {
         echo $user_category."<br>";
         // echo " after : <br>";
         $sql = "INSERT INTO `users`(`name`, `email`, `password`, `gender`, `mobile`, `image`,`user_type`, `birthday`,`status`,`user_category`)
-        VALUES ('$name', '$email', '$password','$gender','$mobileno','$imgContent','$usertype','$birthday',1,'$userCategory')";
+        VALUES ('$name', '$email', '$password','$gender','$mobile','$image','$user_type','$birthday','$status','$user_category')";
         //  // echo "after the sql querys"."<br>";
          if($conn->query($sql) === TURE) {
            echo "<script type='text/javascript'>alert('INSERT INTO feedback Sucessfull!');</script>";
