ba08c1f8528e9140e8c9680b3037e70fbc14fd58
Adican12 <adican12@gmail.com>
2019-05-29 19:07:37 +0300

    2

--- bucketsample.php
+++ bucketsample.php
@@ -7,7 +7,6 @@
   <?php
   $default_bucket = CloudStorageTools::getDefaultGoogleStorageBucketName();
   file_put_contents("gs://catifi1/hello_default.txt", $newFileContent);
-
   ?>
 <h1>bucket</h1>
 </body>
