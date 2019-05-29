c7ebc511f998a6bfd21c17c0c6519e5f7dbb98da
YaronDaya <yarondaya12p@gmail.com>
2019-05-29 15:34:16 +0300

    adding a br

--- pasres.php
+++ pasres.php
@@ -21,35 +21,35 @@ include("php/config.php");
             echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-            echo "File is not an image.";
+            echo "File is not an image.<br>";
             $uploadOk = 0;
         }
     }
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        echo "Sorry, file already exists.<br>";
         $uploadOk = 0;
     }
     // Check file size
     if ($_FILES["file"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+        echo "Sorry, your file is too large.<br>";
         $uploadOk = 0;
     }
     // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
         $uploadOk = 0;
     }
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "Sorry, your file was not uploaded.<br>";
     // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
+            echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.<br>";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "Sorry, there was an error uploading your file.<br>";
         }
     }
 ?>
