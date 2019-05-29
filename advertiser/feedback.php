e8f0db7e44e49a1abdb71fd8aac244a74bbfa32b
YaronDaya <yarondaya12p@gmail.com>
2019-05-29 21:34:03 +0300

    try this

--- advertiser/feedback.php
+++ advertiser/feedback.php
@@ -208,12 +208,15 @@ if(isset($_REQUEST['unconfirm']))
 							<?php if($error){?><div class="errorWrap" id="msgshow"><?php echo htmlentities($error); ?> </div><?php }
 				else if($msg){?><div class="succWrap" id="msgshow"><?php echo htmlentities($msg); ?> </div><?php }?>
 				<form method="post">
+					<br>
 					<label class="col-sm-1 control-label"> Please enter the email <span style="color:red">*</span></label>
 					<div class="col-sm-5">
 					<input type="text" name="email" class="form-control"  placeholder="user@gmail.com" required>
 					</div>
+					<br>
 					<input type="submit" onclick="getTheLastId()" class="myButton">Click me to get the banner ID</button>
 				</form>
+
 								<!-- <table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
 									<thead>
 										<tr>
