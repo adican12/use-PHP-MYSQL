4a7487f0a09df73bc41696fe45dd0c77079d54d3
YaronDaya <yarondaya12p@gmail.com>
2019-05-29 15:15:04 +0300

    print_r

--- pasres.php
+++ pasres.php
@@ -11,6 +11,7 @@ include("php/config.php");
     $target_file = $target_dir . basename($_FILES["file"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    print_r($target_file);
     // Check if image file is a actual image or fake image
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["file"]["tmp_name"]);
