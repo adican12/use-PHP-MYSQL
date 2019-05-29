8087c1d66e5bb36a31394197f885a83f05937974
YaronDaya <yarondaya12p@gmail.com>
2019-05-29 13:00:04 +0300

    echo to target_file to tsee the path

--- pasres.php
+++ pasres.php
@@ -3,6 +3,7 @@
   include("php/config.php");
   $target_dir = "https://storage.cloud.google.com/catifi1/";
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  echo $target_file;
   $uploadOk = 1;
   $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
   // Check if image file is a actual image or fake image
