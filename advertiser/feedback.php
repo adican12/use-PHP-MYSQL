c3acfe5a4073dca18a17f381e6a00a9a8e5ed297
YaronDaya <yarondaya12p@gmail.com>
2019-05-29 22:01:54 +0300

    change the query 4

--- advertiser/feedback.php
+++ advertiser/feedback.php
@@ -322,7 +322,7 @@ if(mysqli_num_rows($result) > 0)
 
 	$email = $_POST['email'];
 	echo $email;
-	$sql = "SELECT MAX(user_id),email FROM ad,users WHERE email=$email; ";
+	$sql = "SELECT MAX(user_id),email FROM ad,users WHERE email='$email'; ";
 	$result = $conn->query($sql);
 	if($result === false)
 	{
