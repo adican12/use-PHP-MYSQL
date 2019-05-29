294891788283da4ad8bd1981b56fdf624aa861d2
YaronDaya <yarondaya12p@gmail.com>
2019-05-29 15:20:06 +0300

    adding system function to upload a images

--- pasres.php
+++ pasres.php
@@ -45,7 +45,7 @@ include("php/config.php");
         echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
     } else {
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        if (file_get_contents($_FILES["file"]["tmp_name"], $target_file)) {
             echo "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.";
         } else {
             echo "Sorry, there was an error uploading your file.";
