38a3c66af31ac82df5af2aa2b8c8838c3cfebc6b
YaronDaya <yarondaya12p@gmail.com>
2019-05-30 15:22:13 +0300

    try this

--- advertiser/profile.php
+++ advertiser/profile.php
@@ -202,7 +202,7 @@ if(isset($_POST['submit']))
 
 	</script>
 
-	<script src="https://maps.googleapis.com/maps/api/js?key=" + key +  "&callback=initMap"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=" + key +"&callback=initMap"></script>
 	<!--
 // 	var marker_array = [];
 // 	var map,marker,info_window;
