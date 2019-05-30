2a17943d3aa6f42c116592db44974a5cc4166b5f
YaronDaya <yarondaya12p@gmail.com>
2019-05-30 17:00:23 +0300

    upload a images to database

--- pasres.php
+++ pasres.php
@@ -4,67 +4,114 @@
 
 
 include("includes/config.php");
-
+if(iseet($_POST["insert"])){
+  $image = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
+  $sql = "INSERT INTO `image`(`image`)VALUES('$image');"
+  if($conn->query($sql)) {
+    echo '<script>alert("Image Inserted into Database")</script>';
+  }
+}
     // $storage = new StorageClient();
     // $storage->registerStreamWrapper();
-    $target_dir = "~/";
-    $target_file = $target_dir . basename($_FILES["image"]["name"]);
-    $uploadOk = 1;
-    $name=$_FILES["image"]["name"];
-    //$tempFile = fopen($target_dir, "w") or die("Error: Unable to open file.");
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    $ex = explode('.',$target_file);
-    print_r($ex);
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["image"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".<br>".$name."<br>";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.<br>";
-            $uploadOk = 0;
-        }
-    }
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.<br>";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["image"]["size"] > 500000) {
-        echo "Sorry, your file is too large.<br>";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.<br>";
-    // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["image"]["name"], $target_file)) {
-            echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.<br>";
-        } else {
-            echo "Sorry, there was an error uploading your file.<br>";
-        }
-    }
+    // $target_dir = "~/";
+    // $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    // $uploadOk = 1;
+    // $name=$_FILES["image"]["name"];
+    // //$tempFile = fopen($target_dir, "w") or die("Error: Unable to open file.");
+    // $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // $ex = explode('.',$target_file);
+    // print_r($ex);
+    // // Check if image file is a actual image or fake image
+    // if(isset($_POST["submit"])) {
+    //     $check = getimagesize($_FILES["image"]["tmp_name"]);
+    //     if($check !== false) {
+    //         echo "File is an image - " . $check["mime"] . ".<br>".$name."<br>";
+    //         $uploadOk = 1;
+    //     } else {
+    //         echo "File is not an image.<br>";
+    //         $uploadOk = 0;
+    //     }
+    // }
+    // // Check if file already exists
+    // if (file_exists($target_file)) {
+    //     echo "Sorry, file already exists.<br>";
+    //     $uploadOk = 0;
+    // }
+    // // Check file size
+    // if ($_FILES["image"]["size"] > 500000) {
+    //     echo "Sorry, your file is too large.<br>";
+    //     $uploadOk = 0;
+    // }
+    // // Allow certain file formats
+    // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    // && $imageFileType != "gif" ) {
+    //     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+    //     $uploadOk = 0;
+    // }
+    // // Check if $uploadOk is set to 0 by an error
+    // if ($uploadOk == 0) {
+    //     echo "Sorry, your file was not uploaded.<br>";
+    // // if everything is ok, try to upload file
+    // } else {
+    //     if (move_uploaded_file($_FILES["image"]["name"], $target_file)) {
+    //         echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.<br>";
+    //     } else {
+    //         echo "Sorry, there was an error uploading your file.<br>";
+    //     }
+    // }
 ?>
 
 <!DOCTYPE html>
 <html>
 <body>
 
-<form action="" method="post" enctype="multipart/form-data">
+<form method="post" enctype="multipart/form-data">
   <br>
     Select image to upload:
-    <input type="file" name="image">
-    <input type="submit" value="Upload" name="submit">
+    <input type="file" name="image" id="image">
+    <input type="submit" value="Insert" name="insert" id="insert">
 </form>
+<br>
+<br>
+<table>
+  <tr>
+    <th>Image</th>
+  </tr>
+  <?php
+  include('includes/config.php');
+  $query = "SELECT * FROM image ORDER BY image_id DESC";
+  if($res = $conn->query($query)) {
+    echo '<script>alert("we print the image")</script>';
+  }
+  while($row = mysqli_fetch_assoc($res)) {
+    echo '<tr>
+              <td>
+                <img src = data:image/jpeg;base64'.base64_encode($row['image']).'""/>;
+              </td>
+          </tr>'
+  }
 
+
+  ?>
+</table>
 </body>
 </html>
+
+<script>
+  $(document).ready(function(){
+    $('#insert').click(function(){
+      var image_name = $('#image').val();
+      if(image_name == ''){
+        alert("Please Select Image");
+        return false;
+      }else {
+        var extension = $('#image').val().split('.').pop().toLowerCase();
+        if(jQuery.inArray(extension),['gif','png','jpg','jpeg'] == -1){
+          alert('Invaild image file');
+          $('#image').val('');
+          return false;
+        }
+      }
+    });
+  });
+</script>
