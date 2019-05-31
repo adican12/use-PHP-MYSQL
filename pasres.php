e2399f292f7ed212b270c733885524b17406096e
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 02:21:58 +0300

    new file

--- pasres.php
+++ pasres.php
@@ -1,12 +1,29 @@
+<?php
+include('inclueds/config.php');
+if(isset($_POST['submit'])) {
+    if(getimagesize($_FILES['imagefile']['tmp_name']) == false){
+      echo ' <br> Please Select An Image.<br>';
+
+    } else {
+      // code...
+    }{
+
+    }
+}
+?>
+
+
 
 <html>
 <head>
   <title>Try to upload a blob image to my sql </title>
+  <meta charset="UTF-8">
 </head>
 <body>
-<form method="POST" action="upload.php" enctype="multipart/form-data">
- <input type="file" name="myimage">
- <input type="submit" name="submit_image" value="Upload">
+<form method="post" action="" enctype="multipart/form-data">
+ <input type="file" name="imagefile">
+ <br>
+ <input type="submit" name="submit" value="Upload">
 </form>
 
 </body>
