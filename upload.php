66c492edadcca61ab07556a4ad207c1bd27c05ab
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 00:37:08 +0300

    maybe this

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ $imagename=$_FILES["myimage"]["name"];
 
 //Get the content of the image and then add slashes to it
 $imagetmp=addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
-
+echo $imagetmp;
 //Insert the image name and image content in image_table
 $insert_image="INSERT INTO image VALUES('$imagetmp','$imagename')";
 
