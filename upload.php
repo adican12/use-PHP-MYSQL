13ecefd1ceb06ef735cfa36b55d3c4d9790e199a
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 00:35:16 +0300

    try now

--- upload.php
+++ upload.php
@@ -9,5 +9,7 @@ $imagetmp=addslashes (file_get_contents($_FILES['myimage']['tmp_name']));
 //Insert the image name and image content in image_table
 $insert_image="INSERT INTO image VALUES('$imagetmp','$imagename')";
 
-mysql_query($insert_image);
+if(mysql_query($insert_image)){
+  echo "we add the image to table;";
+}
 ?>
