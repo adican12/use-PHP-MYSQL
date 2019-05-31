7b6b9b04f9b1d1269ada0aa9a0e0447295f5002e
YaronDaya <yarondaya12p@gmail.com>
2019-05-31 12:06:37 +0300

    try this back

--- advertiser/profile.php
+++ advertiser/profile.php
@@ -212,20 +212,12 @@ console.log("the length is : " + length);
 			map:map
 
 		});
-		var marker2 = new google.maps.Marker({
-			position:{lat:<?php echo $info['lat']?>,lng:<?php echo $info['lng']?>},
-			map:map
-
-		});
 
 		var infowindow= new google.maps.InfoWindow({
 			content:'<h3>HERE WE HAVE A WIFI YOU CAN PUBLISH HERE</h3>'
 		});
 		//adding a new infowindow
 		var infowindow1 = new google.maps.InfoWindow({
-			content: '<h3><?php echo $result['info'].$result['name']?>;</h3>'
-		});
-		var infowindow2 = new google.maps.InfoWindow({
 			content: '<h3><?php echo $result['info'].$result['name']?>;</h3>'
 		});
 		// add a listnerr when the click we see the msg.
