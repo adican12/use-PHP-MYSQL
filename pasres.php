3ea113190ded1a4659a1882709c5b1af2c8907a4
YaronDaya <yarondaya12p@gmail.com>
2019-05-31 23:53:22 +0300

    adding image to db

--- pasres.php
+++ pasres.php
@@ -1,53 +1,20 @@
 
-
 <?php
   include('includes/config.php');
-$target_dir = "images/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+  if(isset($_POST['uploadfilesub'])) {
+    $filename=$_FILES['uploadfile']['name'];
+    $filetempname=$_FILES['uploadfile']['tmp_name'];
+    $folder = 'images/';
+    if(move_uploaded_file($filetempname,$folder.$filename) === TRUE){
+  $sql = "INSERT INTO `image`(`image`)VALUES('$filename');";
+  if($qry = $conn->query($sql)) {
+    //echo "image uploaded";
+  }
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        $sql = "INSERT INTO `image`(`image`)VALUES('$target_file');";
-        if($qry = $conn->query($sql)) {
-          echo "image uploaded";
-        }
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+  echo "ERROR_THE_FILE_NOT_IN_FOLDER";
 }
+}
+$image_id = 1;
 $sql = "SELECT * FROM image";
 if($res = $conn->query($sql)) {
   //echo '<script>alert("ok the query is working work")</script>';
@@ -55,15 +22,16 @@ if($res = $conn->query($sql)) {
     echo '<img height = 150px width=250px src="'.$row['image'].'">';
   }
 }
+
+
 ?>
 <!DOCTYPE html>
 <html>
 <body>
-  <form action="" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
+    <form action="" method="post" enctype="multipart/form-data">
+      <input type="file" name="uploadfile" />
+      <input type="submit" name="uploadfilesub" value="upload" />
+    </form>
     <table style="width:100%">
   <tr>
     <th>Image</th>
