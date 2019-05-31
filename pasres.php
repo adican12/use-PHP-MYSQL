68eca0346b25130fd5cd7bf424559ea7c873f04f
YaronDaya <yarondaya12p@gmail.com>
2019-05-31 23:04:59 +0300

    try this upload image to db and show it 2

--- pasres.php
+++ pasres.php
@@ -1,60 +1,42 @@
 
 <?php
-include('includes/config.php');
-if(isset($_POST['submit'])) {
-	$filename=$_FILES['uploadfile']['name'];
-	    $filetempname=$_FILES['uploadfile']['tmp_name'];
-	    $folder = 'images/';
-	    move_uploaded_file($filetempname,$folder.$filename);
-	  $sql = "INSERT INTO `image`(`image`)VALUES('$filename');";
-	  if($qry = $conn->query($sql)) {
-	    echo "image uploaded";
-	  }
-	}
-	$sql = "SELECT * FROM image WHERE image_id =";
-	if($res = $conn->query($sql)) {
-	  echo '<script>alert("ok the query is working work")</script>';
-	}
-	while($row =  mysqli_fetch_assoc($res)) {
-	  echo '<img height = 150px width=100px src="'.$row['image'].'">';
-	}
-$conn->close();
-?>
+  include('includes/config.php');
+  if(isset($_POST['uploadfilesub'])) {
+    $filename=$_FILES['uploadfile']['name'];
+    $filetempname=$_FILES['uploadfile']['tmp_name'];
+    $folder = 'images/';
+    move_uploaded_file($filetempname,$folder.$filename);
+  $sql = "INSERT INTO `image`(`image`)VALUES('$filename');";
+  if($qry = $conn->query($sql)) {
+    echo "image uploaded";
+  }
+}
+$sql = "SELECT * FROM image WHERE image_id =23";
+if($res = $conn->query($sql)) {
+  echo '<script>alert("ok the query is working work")</script>';
+}
+while($row =  mysqli_fetch_assoc($res)) {
+  echo '<img height = 150px width=250px src="'.$row['image'].'">';
+}
 
+?>
 <!DOCTYPE html>
 <html>
 <body>
-
-<form method="post" enctype="multipart/form-data">
-  <br>
-    Select image to upload:
-    <input type="file" name="uploadfile" id="uploadfile">
-    <input type="submit" value="Insert" name="insert" id="insert">
-</form>
-<br>
-<br>
-<table>
+    <form action="" method="post" enctype="multipart/form-data">
+      <input type="file" name="uploadfile" />
+      <input type="submit" name="uploadfilesub" value="upload" />
+    </form>
+    <table style="width:100%">
   <tr>
     <th>Image</th>
   </tr>
-  <?php
-  include('includes/config.php');
-  $query = "SELECT * FROM image ORDER BY image_id DESC";
-  if($res = $conn->query($query)) {
-    echo '<script>alert("we print the image")</script>';
-  }
-  while($row = mysqli_fetch_assoc($res)) {
-    echo '<tr>
-              <td>'
-              echo '<img height = 150px width=100px src="'.$row['image'].'">;
-              </td>
-          </tr>';
-  }
-  ?>
+  <tr>
+    <td><?php echo $row['image'];?></td>
+  </tr>
 </table>
 </body>
 </html>
-
 <script>
   $(document).ready(function(){
     $('#insert').click(function(){
