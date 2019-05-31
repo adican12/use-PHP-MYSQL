8eb317ead802e89a9746d3553b0606b8f2b4f811
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 00:14:03 +0300

    try this

--- pasres.php
+++ pasres.php
@@ -32,3 +32,20 @@ if($res = $conn->query($sql)) {
     <input type="file" name="file">
     <input type="submit" name="submit" value="Upload">
 </form>
+
+<?php
+// Include the database configuration file
+include 'includes/config.php';
+
+// Get images from the database
+$query = $db->query("SELECT * FROM images ORDER BY uploaded_on DESC");
+
+if($query->num_rows > 0){
+    while($row = $query->fetch_assoc()){
+        $imageURL = 'uploads/'.$row["file_name"];
+?>
+    <img src="<?php echo $imageURL; ?>" alt="" />
+<?php }
+}else{ ?>
+    <p>No image(s) found...</p>
+<?php } ?>
