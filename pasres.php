7f0ba1481cc6ae28eb8a5987b8b1794d79d4dcd6
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 02:21:14 +0300

    new file

--- pasres.php
+++ pasres.php
@@ -1,7 +1,9 @@
 
 <html>
+<head>
+  <title>Try to upload a blob image to my sql </title>
+</head>
 <body>
-
 <form method="POST" action="upload.php" enctype="multipart/form-data">
  <input type="file" name="myimage">
  <input type="submit" name="submit_image" value="Upload">
