a8da44889cad44f05431687d19b61dcb41b4ab4b
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 12:19:39 +0300

    maybe now

--- pasres.php
+++ pasres.php
@@ -52,7 +52,7 @@ echo " hello from here <br>";
    } else {
      while($row=mysqli_fetch_assoc($res)){
        //echo " we in the while";
-      echo '<img height = 150px width=100px src=data:image;"'.$row['image'].'">';
+      echo '<img height = 150px width=100px src=data:image/jpg;base64;"'.$row['image'].'">';
       }
    }
   //   while($row=mysqli_fetch_assoc($res))
