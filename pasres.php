294c5744ff90ea4b380acaf51c10a359062d6b04
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 12:40:48 +0300

    try

--- pasres.php
+++ pasres.php
@@ -52,7 +52,7 @@ echo " hello from here <br>";
      while($row=mysqli_fetch_assoc($res)){
        //echo " we in the while";
        echo base64_decode($row['image']);
-      echo '<img height = 150px width=100px src=data:image;base64,'.$row['image'].'>';
+      echo '<img height = 150px width=100px src=data:image;base64_decode,'.$row['image'].'>';
       echo "what happend";
       }
    }
