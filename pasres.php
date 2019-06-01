186003381b9643038b052c240f529e599d0faa35
YaronDaya <yarondaya12p@gmail.com>
2019-06-01 12:34:45 +0300

    try this

--- pasres.php
+++ pasres.php
@@ -51,6 +51,7 @@ echo " hello from here <br>";
    } else {
      while($row=mysqli_fetch_assoc($res)){
        //echo " we in the while";
+       echo $row['image'];
       echo '<img height = 150px width=100px src=data:image;base64_encode,"'.$row['image'].'">';
       echo "what happend";
       }
