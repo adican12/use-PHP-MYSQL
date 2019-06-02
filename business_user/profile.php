8785bd5d8897219175b88ca7f86c547d4331d762
YaronDaya <yarondaya12p@gmail.com>
2019-06-02 18:13:02 +0300

    try this to

--- business_user/profile.php
+++ business_user/profile.php
@@ -266,7 +266,7 @@ var data = google.visualization.arrayToDataTable([
 ]);
 
 // Optional; add a title and set the width and height of the chart
-var options = {'title':'Age', 'width':550, 'height':400};
+var options = {'title':'Age', 'width':550, 'height':400,'is3D': true};
 
 // Display the chart inside the <div> element with id="piechart"
 var chart = new google.visualization.PieChart(document.getElementById('piechart1'));
