176c0ad085eff89034a24093a1120b6b44e4aa64
YaronDaya <yarondaya12p@gmail.com>
2019-06-02 20:22:44 +0300

    try the table now;

--- business_user/download.php
+++ business_user/download.php
@@ -131,7 +131,6 @@ if(mysqli_num_rows($result) > 0)
 	while($row = mysqli_fetch_assoc($result)) {
 
 	?>
-        <h5 style="background:#ededed;padding:20px;"><i class="fa fa-bell text-primary"></i>&nbsp;&nbsp;<b class="text-primary"><?php echo $row['time'];?></b> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 					<?php echo $row['notiuser'];?> <?php echo $row['notitype'];?></h5>
                        <?php $cnt=$cnt+1; }} ?>
 											 <table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
