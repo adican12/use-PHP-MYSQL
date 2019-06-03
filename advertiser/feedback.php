5cb9b4a96f45fe5043cfddc83123e5ef300c6cf0
YaronDaya <yarondaya12p@gmail.com>
2019-06-03 13:38:14 +0300

    change css

--- advertiser/feedback.php
+++ advertiser/feedback.php
@@ -291,6 +291,7 @@ if(mysqli_num_rows($result) > 0)
 	 <div class="form-content">
 			<form id="ad" method="post">
 				<!-- <input type="file" name="file" value="Please select a picture" id="fileToUpload"> -->
+					<label for="" class="text-uppercase text-sm"> Image:</label>
 				<input type="file" name="imagefile" value="image" style="padding:10px"><br>
 				<label for="" class="text-uppercase text-sm"> Price:</label>
 				<input type="text" placeholder="Price:" name="price" class="form-control mb" required>
