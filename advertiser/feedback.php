81e2d6c45e907c4ec9ecd951d8f280dfff04c193
YaronDaya <yarondaya12p@gmail.com>
2019-06-03 14:37:58 +0300

    try it now

--- advertiser/feedback.php
+++ advertiser/feedback.php
@@ -370,8 +370,8 @@ if(mysqli_num_rows($result) > 0)
 			var header = "<?php echo $new_row['header'];?>";
 			document.getElementById("header").innerHTML = header;
 
-			let image = <?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $new_row['image'] ).'"/>';?>
-			document.getElementById("img").src= image;
+
+			document.getElementById("img").src= '<?php echo '<img src="data:image/jpeg;base64,'.base64_encode( $new_row['image'] ).'"/>';?>'
 
 
 	}
