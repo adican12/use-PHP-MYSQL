926da9152aad0b4dcde21da152614970617c8712
YaronDaya <yarondaya12p@gmail.com>
2019-06-03 14:21:42 +0300

    try it now

--- advertiser/feedback.php
+++ advertiser/feedback.php
@@ -362,7 +362,7 @@ if(mysqli_num_rows($result) > 0)
 			//change the price
 			var price = <?php echo $new_row['price'];?>;
 			document.getElementById("price").innerHTML =  price + " $ ";
-			var text = <?php echo $new_row['text'];?>;
+			let text = <?php echo $new_row['text'];?>;
 			document.getElementById("details").innerHTML = text;
 
 
