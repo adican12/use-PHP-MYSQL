a3347c3fb26f1e026450753b03ae818f1dc9f180
YaronDaya <yarondaya12p@gmail.com>
2019-06-05 12:25:31 +0300

    check it

--- imageUpload.php
+++ imageUpload.php
@@ -26,11 +26,9 @@ $counter = 0;
 
 
   	if (move_uploaded_file($_FILES['image']['tmp_name'], "$target") == false) {
-  		echo "Image uploaded successfully <br>";
-
-  	}else{
-  		 echo  "Failed to upload image <br>";
-
+        echo  "Failed to upload image <br>";
+  	} else {
+	       echo "Image uploaded successfully <br>";
   	}
   }
   }
