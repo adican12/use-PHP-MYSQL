ebf9e40f5c8838efdfbe9f994d0a48800ecb728e
YaronDaya <yarondaya12p@gmail.com>
2019-06-05 11:50:59 +0300

    change the traget dir

--- imageUpload.php
+++ imageUpload.php
@@ -14,7 +14,7 @@
   	$image_text = mysqli_real_escape_string($conn, $_POST['image_text']);
 
   	// image file directory
-  	$target = "images/".basename($image);
+  	$target = "../images/".basename($image);
 
   	$sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
   	// execute query
