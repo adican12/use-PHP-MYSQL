4aa79e30dd58040f7e9513683c069ce3801e5a85
YaronDaya <yarondaya12p@gmail.com>
2019-06-05 12:01:56 +0300

    change the table in db

--- catifi.sql
+++ catifi.sql
@@ -370,7 +370,7 @@ CREATE TABLE `br` (
 /*--------------example images*/
 CREATE TABLE `images` (
   `image_id` INT(11) AUTO_INCREMENT NOT NULL,
-  `image` VARCHAR(255) NOT NULL,
+  `image` VARCHAR(100) NOT NULL,
   `image_test` TEXT NOT NULL,
   PRIMARY KEY(image_id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
