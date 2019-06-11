0e24949daa93fdca6e91e31fd853b6843a70821a
YaronDaya <yarondaya12p@gmail.com>
2019-06-11 19:22:24 +0300

    adding css to container

--- business_user/feedback.php
+++ business_user/feedback.php
@@ -126,6 +126,7 @@ if(isset($_REQUEST['unconfirm']))
     -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
 }
+#container {height: 370px; width: 100%;}
 
 		</style>
 
