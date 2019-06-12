46003ca1b7764f71c5762f73295dde4bfd25251e
Adican12 <adican12@gmail.com>
2019-06-12 20:23:37 +0300

    php google client

--- test_bucket.php
+++ test_bucket.php
@@ -2,22 +2,22 @@
 # Includes the autoloader for libraries installed with composer
 require __DIR__ . '/vendor/autoload.php';
 
-# Imports the Google Cloud client library
-use Google\Cloud\Storage\StorageClient;
+// # Imports the Google Cloud client library
+// use Google\Cloud\Storage\StorageClient;
+//
+// # Your Google Cloud Platform project ID
+// $projectId = 'YOUR_PROJECT_ID';
+//
+// # Instantiates a client
+// $storage = new StorageClient([
+//     'projectId' => $projectId
+// ]);
+//
+// # The name for the new bucket
+// $bucketName = 'my-new-bucket';
+//
+// # Creates the new bucket
+// $bucket = $storage->createBucket($bucketName);
 
-# Your Google Cloud Platform project ID
-$projectId = 'YOUR_PROJECT_ID';
-
-# Instantiates a client
-$storage = new StorageClient([
-    'projectId' => $projectId
-]);
-
-# The name for the new bucket
-$bucketName = 'my-new-bucket';
-
-# Creates the new bucket
-$bucket = $storage->createBucket($bucketName);
-
-echo 'Bucket ' . $bucket->name() . ' created.';
+// echo 'Bucket ' . $bucket->name() . ' created.';
 ?>
