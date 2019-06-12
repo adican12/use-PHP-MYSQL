810018c9b7ab4c8247aedc8b4e8104d6ba673d8c
YaronDaya <yarondaya12p@gmail.com>
2019-06-12 21:43:29 +0300

    change db

--- catifi3.sql
+++ catifi3.sql
@@ -87,7 +87,6 @@ INSERT INTO `users` (`user_id`, `name`, `email`, `password`, `gender`, `mobile`,
 create table if not exists `business` (
   `businessID` int(11) NOT NULL AUTO_INCREMENT,
   `category` varchar(250) NOT NULL,
-  `locationID` int(11) NOT NULL,
   `business_name` varchar(250) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY(`businessID`)
