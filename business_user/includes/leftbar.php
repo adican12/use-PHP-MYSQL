8d8801a468e630f426e9f7ef514d0daccd3e6011
YaronDaya <yarondaya12p@gmail.com>
2019-06-12 19:22:23 +0300

    change it

--- business_user/includes/leftbar.php
+++ business_user/includes/leftbar.php
@@ -4,6 +4,8 @@
 				<li class="ts-label">Main</li>
 				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard </a></li>
 
+				<li><a href "sendreply.php"><i class="fas fa-store"></i> Business Manager </a></li>
+
 			<li><a href="userlist.php"><i class="fa fa-users"></i> Meet Your  Customers</a>
 			</li>
 			<li><a href="profile.php"><i class="fa fa-area-chart" aria-hidden="true"></i> &nbsp;Statics</a>
