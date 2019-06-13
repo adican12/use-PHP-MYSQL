360abdb233029dfc5d0a13f5f5034fad96e7c24e
YaronDaya <yarondaya12p@gmail.com>
2019-06-13 21:36:24 +0300

    try now

--- test_bucket.php
+++ test_bucket.php
@@ -58,7 +58,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if ($bucket->upload(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"])) {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
