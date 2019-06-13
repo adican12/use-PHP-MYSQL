c4b77a7b43fa6a42ec839c1531f987f3a7598eda
Adican12 <adican12@gmail.com>
2019-06-13 21:54:23 +0300

    bucket

--- test_bucket.php
+++ test_bucket.php
@@ -22,51 +22,56 @@ $bucket = $storage->bucket('catifi2');
 
 
 $target_dir = "newImages/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+// $uploadOk = 1;
+// $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-  $bucket->upload($_FILES["fileToUpload"]["tmp_name"]);
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+  $file= $_FILES["fileToUpload"];
+  $bucket->upload($file);
+    // $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    // if($check !== false) {
+    //     echo "File is an image - " . $check["mime"] . ".";
+    //     $uploadOk = 1;
+    // } else {
+    //     echo "File is not an image.";
+    //     $uploadOk = 0;
+    // }
 }
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
+// if (file_exists($target_file)) {
+//     echo "Sorry, file already exists.";
+//     $uploadOk = 0;
+// }
+// // Check file size
+// if ($_FILES["fileToUpload"]["size"] > 500000) {
+//
+//     echo "Sorry, your file is too large.";
+//     $uploadOk = 0;
+// }
+// // Allow certain file formats
+// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+// && $imageFileType != "gif" ) {
+//     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//     $uploadOk = 0;
+// }
+// // Check if $uploadOk is set to 0 by an error
+// if ($uploadOk == 0) {
+//     echo "Sorry, your file was not uploaded.";
+// // if everything is ok, try to upload file
+// } else {
+//     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+//     } else {
+//         echo "Sorry, there was an error uploading your file.";
+//     }
+// }
 ?>
+<html>
+<header>
 
+</header>
+<body>
 <h1>bucket</h1>
 
 <form action="" method="post" enctype="multipart/form-data">
@@ -74,7 +79,5 @@ if ($uploadOk == 0) {
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submit">
 </form>
-
-
 </body>
 </html>
