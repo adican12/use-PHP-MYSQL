7e324520e6ad183d68b57cd1761df1183904a7ff
YaronDaya <yarondaya12p@gmail.com>
2019-06-13 17:46:24 +0300

    change js

--- business_user/userlist.php
+++ business_user/userlist.php
@@ -522,10 +522,10 @@ chart.draw(data, options);
 
 function myFunction() {
   var x = document.getElementById("myDIV");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
 
