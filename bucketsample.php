1658c1805909c115b049fe4cfe92350e76e2896c
YaronDaya <yarondaya12p@gmail.com>
2019-06-13 18:37:18 +0300

    try now

--- bucketsample.php
+++ bucketsample.php
@@ -5,7 +5,7 @@
 <body>
 
   <?php
-  $target_dir = "uploads/";
+  $target_dir = "newImages/";
   $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
   $uploadOk = 1;
   $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
