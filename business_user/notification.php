689349b1d957cdd235399d5e12c3ac3a001f516e
YaronDaya <yarondaya12p@gmail.com>
2019-06-13 18:11:14 +0300

    try this now

--- business_user/notification.php
+++ business_user/notification.php
@@ -94,25 +94,11 @@ if(isset($_POST['addCoupon'])) {
 	<!-- adding w3schools card -->
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
+	<!-- notification style -->
+	<link rel ="stylesheet" href ="css/noti.css">
+
 	<script type= "text/javascript" src="../vendor/countries.js"></script>
-	<style>
-	.errorWrap {
-    padding: 10px;
-    margin: 0 0 20px 0;
-	background: #dd3d36;
-	color:#fff;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-}
-.succWrap{
-    padding: 10px;
-    margin: 0 0 20px 0;
-	background: #5cb85c;
-	color:#fff;
-    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
-}
-		</style>
+
 
 		<!-- Loading Scripts -->
 		<script src="js/jquery.min.js"></script>
