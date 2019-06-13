e059c281dbb75b5f48b610a60c5debc0a2307251
YaronDaya <yarondaya12p@gmail.com>
2019-06-13 19:56:47 +0300

    try it

--- business_user/includes/config.php
+++ business_user/includes/config.php
@@ -13,22 +13,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-//# Includes the autoloader for libraries installed with composer
-require __DIR__ . '/vendor/autoload.php';
 
-// # Imports the Google Cloud client library
-use Google\Cloud\Storage\StorageClient;
-//
-// # Your Google Cloud Platform project ID
-$projectId = 'catifi';
-//
-// # Instantiates a client
-$storage = new StorageClient([
-    'projectId' => $projectId
-]);
-//
-// // # The name for the new bucket
-// $bucketName = 'my-new-bucket';
-// //
-$bucket = $storage->bucket('catifi2');
 ?>
