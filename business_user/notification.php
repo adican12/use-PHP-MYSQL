c028ceb69efe641d72fa0edd12076fa54a332660
YaronDaya <yarondaya12p@gmail.com>
2019-06-14 12:29:38 +0300

    try this

--- business_user/notification.php
+++ business_user/notification.php
@@ -219,4 +219,27 @@ if(mysqli_num_rows($result) > 0)
 </div>
 </body>
 </html>
+
+<script>
+			$("#createCoupon").submit(function(){
+			//alert("signup_form");
+			$("button").prop('disabled', true);
+			var formData = new FormData(this);
+			$.ajax({
+				url:     '../test_bucket.php',
+				type:    'POST',
+				data:    formData,
+				async:   false,
+				success: function(data) {
+					alert("success");
+				   	$(".panel-body").html(data);
+					$("button").prop('disabled', false);
+				},
+				cache: false,
+				contentType: false,
+				processData: false
+			});
+			return false;
+	});
+	</script>
 <?php } ?>
