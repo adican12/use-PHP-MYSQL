541dd8b9b81bf042a062ef326a530ac5045ac6d0
YaronDaya <yarondaya12p@gmail.com>
2019-06-15 22:44:50 +0300

    try this

--- advertiser/feedback.php
+++ advertiser/feedback.php
@@ -123,6 +123,50 @@ if(isset($_REQUEST['unconfirm']))
 	<!-- main.css -->
 	<link rel="stylesheet" href ="css/main.css">
 
+
+	<!-- Loading Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+	<script type="text/javascript">
+				 $(document).ready(function () {
+					setTimeout(function() {
+						$('.succWrap').slideUp("slow");
+					}, 3000);
+//add to database new ad
+
+			$("#ad").submit(function(){
+			//alert("signup_form");
+			$("button").prop('disabled', true);
+			var formData = new FormData(this);
+			$.ajax({
+				url:     'insert_ad.php',
+				type:    'POST',
+				data:    formData,
+				async:   false,
+				success: function(data) {
+					alert("success");
+						$("#result_ad").html(data);
+					$("button").prop('disabled', false);
+				},
+				cache: false,
+				contentType: false,
+				processData: false
+			});
+			return false;
+	});
+	$("#formButton").click(function(){
+				$("#information").toggle();
+		});
+					});
+		</script>
+
 </head>
 
 <body>
@@ -394,7 +438,6 @@ function myFunction() {
 }*/
 </script>
 
-	<!-- Loading Scripts -->
 
 </body>
 </html>
