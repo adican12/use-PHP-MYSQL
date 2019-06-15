2e36ec360409dda8e1ceb1b9e2ff38ab0929cd79
YaronDaya <yarondaya12p@gmail.com>
2019-06-15 22:46:02 +0300

    try this

--- advertiser/feedback.php
+++ advertiser/feedback.php
@@ -134,37 +134,9 @@ if(isset($_REQUEST['unconfirm']))
 	<script src="js/fileinput.js"></script>
 	<script src="js/chartData.js"></script>
 	<script src="js/main.js"></script>
+	<script src="js/function.js"></script>
 	<script type="text/javascript">
-				 $(document).ready(function () {
-					setTimeout(function() {
-						$('.succWrap').slideUp("slow");
-					}, 3000);
-//add to database new ad
 
-			$("#ad").submit(function(){
-			//alert("signup_form");
-			$("button").prop('disabled', true);
-			var formData = new FormData(this);
-			$.ajax({
-				url:     'insert_ad.php',
-				type:    'POST',
-				data:    formData,
-				async:   false,
-				success: function(data) {
-					alert("success");
-						$("#result_ad").html(data);
-					$("button").prop('disabled', false);
-				},
-				cache: false,
-				contentType: false,
-				processData: false
-			});
-			return false;
-	});
-	$("#formButton").click(function(){
-				$("#information").toggle();
-		});
-					});
 		</script>
 
 </head>
