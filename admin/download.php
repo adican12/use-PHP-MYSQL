1e7de08f47dbff63a277aa312ba905eadd9ba98d
YaronDaya <yarondaya12p@gmail.com>
2019-06-16 13:24:22 +0300

    try

--- admin/download.php
+++ admin/download.php
@@ -87,7 +87,7 @@ header("Content-type: application/octet-stream");
 header("Content-Disposition: attachment; filename=".$filename."-report.xls");
 header("Pragma: no-cache");
 header("Expires: 0");
-			
+			$cnt++;
 			}
 	}
 ?>
