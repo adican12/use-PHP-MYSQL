4179a2eb807bf2f0c14a623144bd48bd2326ed63
YaronDaya <yarondaya12p@gmail.com>
2019-06-18 16:34:21 +0300

    try th

--- messages.php
+++ messages.php
@@ -119,13 +119,14 @@ foreach($results as $result)
 		</div>
 	</div>
 <?php
+echo "<script>alert('adi the cat')</script>";
 	$sql = "SELECT coupon.imageURL,coupon.counter,coupon.couponName,users_coupon.user_id FROM coupon,users_coupon WHERE coupon.couponID =users_coupon.coupon_id";
 	$result = $conn->query($sql);
 	if($result === false) {
 		user_error("Query failed: ".$conn->error."<br />$sql");
 		echo "false";
 	}
-	echo "<script>alert('adi the cat')</script>";
+
 	$row = mysqli_num_rows($result);
 ?>
 
