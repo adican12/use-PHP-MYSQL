f05eb1100873e4112e5378c77057920cfc0ad5b0
YaronDaya <yarondaya12p@gmail.com>
2019-06-18 23:10:21 +0300

    test

--- notification.php
+++ notification.php
@@ -95,7 +95,7 @@ else{
 
 											 <div class="card">
 														 <img src="<?php echo $row['image']?>" id="img" style="width:100%">
-														 <h1 id="header"><?php echo $row['title']?></h1>
+														 <h1 id="header" class="title"><?php echo $row['title']?></h1>
 													   <p class="price" id="price"><?php echo $row['price']?>$</p>
 													   <p id="details"><?php echo $row['description']?></p>
 												 </div>
@@ -103,15 +103,15 @@ else{
 
 											<div class="card1">
 													<img src="<?php echo $row2['image']?>" id="img1" style="width:100% ">
-													<h1 id="header" style="font-size:18px;"><?php echo $row2['title']?></h1>
+													<h1 id="header" class="title"><?php echo $row2['title']?></h1>
 													<p class="price" id="price"><?php echo $row2['price']?>$</p>
 													<p id="details"><?php echo $row2['description']?></p>
 											</div>
 											<br>
 
-											<div class="card2" id="img2" style="width:100%">
+											<div class="card2" >
 												<img src="<?php echo $row3['image']?>" style="width:100%">
-												<h1 id="header"> <?php echo $row3['title']?></h1>
+												<h1 id="header" class="title"> <?php echo $row3['title']?></h1>
 												<p class="price" id="price"><?php echo $row3['price']?> $</p>
 												<p id="details"><?php echo $row3['description']?></p>
 											</div>
