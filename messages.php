5adf6a5eaed637eb18962c5056bb740ff0ffc155
YaronDaya <yarondaya12p@gmail.com>
2019-06-18 17:31:51 +0300

    maybe now

--- messages.php
+++ messages.php
@@ -77,10 +77,10 @@ else{
 
 			function changeDetails(){
 
-				var x = '<?php $row['imageURL']?>';
+				var x = <?php $row['imageURL']?>;
 				console.log(x);
-				document.getElementById("img").src ='<?php echo $row['imageURL']?>;'
-				document.getElementById("couponName").innerHTML ='someting';
+				document.getElementById("img").src =<?php echo $row['imageURL']?>;
+				document.getElementById("couponName").innerHTML ='<?php echo $row['couponName']?>';
 				document.getElementById("counter").innerHTML =<?php echo  $row['counter']?>;
 			}
 		</script>
