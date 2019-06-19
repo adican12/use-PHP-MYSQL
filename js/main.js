0e2ab3a9b45a8c1844f88d75ddcb904c2fd4009d
YaronDaya <yarondaya12p@gmail.com>
2019-06-19 15:32:14 +0300

    try this

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
  $(document).ready(function () {
-	 
+
  	$(".ts-sidebar-menu li a").each(function () {
  		if ($(this).next().length > 0) {
  			$(this).addClass("parent");
@@ -17,11 +17,11 @@
  	$('.menu-btn').click(function () {
  		$('nav.ts-sidebar').toggleClass('menu-open');
  	});
-	 
-	 
+
+
 	 $('#zctb').DataTable();
-	 
-	 
+
+
 	 $("#input-43").fileinput({
 		showPreview: false,
 		allowedFileExtensions: ["zip", "rar", "gz", "tgz"],
@@ -30,3 +30,36 @@
 	});
 
  });
+
+
+ document.addEventListener('DOMContentLoaded', function(){
+ addListeners();
+ setRating(); //based on value inside the div
+ });
+
+ function addListeners(){
+ var stars = document.querySelectorAll('.star');
+ [].forEach.call(stars, function(star, index){
+ 	star.addEventListener('click', (function(idx){
+ 		console.log('adding rating on', index);
+ 		document.querySelector('.stars').setAttribute('data-rating',  idx + 1);
+ 		console.log('Rating is now', idx+1);
+ 		setRating();
+ 	}).bind(window,index) );
+ });
+
+ }
+
+ function setRating(){
+ var stars = document.querySelectorAll('.star');
+ var rating = parseInt( document.querySelector('.stars').getAttribute('data-rating') );
+ [].forEach.call(stars, function(star, index){
+ 	if(rating > index){
+ 		star.classList.add('rated');
+ 		console.log('added rated on', index );
+ 	}else{
+ 		star.classList.remove('rated');
+ 		console.log('removed rated on', index );
+ 	}
+ });
+ }
