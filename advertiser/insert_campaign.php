4bfa74f57152bed3989dab304aac96e19f8b9ccb
YaronDaya <yarondaya12p@gmail.com>
2019-06-19 22:47:15 +0300

    try this

--- advertiser/insert_campaign.php
+++ advertiser/insert_campaign.php
@@ -1,12 +1,9 @@
 <?php
 include('includes/config.php');
  session_start();
-
- ++$_SESSION['id'];
-++$_SESSION['adID'];
+;
 // error_reporting(0);
-  $id = $_SESSION['id'];
- $adID = $_SESSION['adID'];
+
 
 	$campaignName = $_POST['campaignName'];
 	$budget = $_POST['budget'];
@@ -16,15 +13,17 @@ include('includes/config.php');
 	$category = $_POST['category'];
 	$ageMin = $_POST['ageMin'];
 	$ageMax = $_POST['ageMax'];
+  $adID = $_POST['adID'];
+  $location=$_POST['location_id'];
 
-	echo "the id is = ".$id."<br>";
+
 	 echo "the adID is = ".$adID."<br>";
    echo "Campagin Name:".$campaignName."<br>"."the budget is : ".$budget."<br>"."the gender is: ".$gender."<br>";
    echo "the strat date of the campaign is : ".$stratDate."<br>"."the end date of the campaign is : ".$endDate."<br>";
    echo "the category is : " .$category;
 
- $sql = "INSERT INTO `campaign`(`campaignName`,`id`,`adID`,`gender`,`ageMin`,`ageMax`,`budget`,`category`,`stratingDate`,`endDate`)
- 	 VALUES('$campaignName','$id','$adID','$gender','$ageMin','$ageMax','$budget','$category','$stratDate','$endDate');";
+ $sql = "INSERT INTO `campaign`(`campaignName`,`adID`,`gender`,`ageMin`,`ageMax`,`budget`,`category`,`stratingDate`,`endDate`,`locationID`)
+ 	 VALUES('$campaignName','$adID','$gender','$ageMin','$ageMax','$budget','$category','$stratDate','$endDate','$location_id');";
  	if ($conn->query($sql) === TRUE) {
  	echo "<script type='text/javascript'>alert('Insert  Sucessfull!');</script>";
  	echo "<script type='text/javascript'> document.location = 'notification.php'; </script>";
