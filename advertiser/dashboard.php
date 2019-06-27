98768bf31669bf9def58424b0bf371e58e4fc87f
Adican12 <adican12@gmail.com>
2019-06-27 18:41:34 +0300

    a

--- advertiser/dashboard.php
+++ advertiser/dashboard.php
@@ -21,21 +21,21 @@ else{
 	<title>Advertiser Dashboard</title>
 
 	<!-- Font awesome -->
-	<link rel="stylesheet" href="/dashboard/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<!-- Sandstone Bootstrap CSS -->
-	<link rel="stylesheet" href="/dashboard/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<!-- Bootstrap Datatables -->
-	<link rel="stylesheet" href="/dashboard/css/dataTables.bootstrap.min.css">
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
 	<!-- Bootstrap social button library -->
-	<link rel="stylesheet" href="/dashboard/css/bootstrap-social.css">
+	<link rel="stylesheet" href="css/bootstrap-social.css">
 	<!-- Bootstrap select -->
-	<link rel="stylesheet" href="/dashboard/css/bootstrap-select.css">
+	<link rel="stylesheet" href="css/bootstrap-select.css">
 	<!-- Bootstrap file input -->
-	<link rel="stylesheet" href="/dashboard/css/fileinput.min.css">
+	<link rel="stylesheet" href="css/fileinput.min.css">
 	<!-- Awesome Bootstrap checkbox -->
-	<link rel="stylesheet" href="/dashboard/css/awesome-bootstrap-checkbox.css">
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
 	<!-- Admin Stye -->
-	<link rel="stylesheet" href="/dashboard/css/style.css">
+	<link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
