8e74926a502a03f103b6a94122110f2d89c3cf13
Adican12 <adican12@gmail.com>
2019-06-27 18:47:41 +0300

    c

--- advertiser/dashboard.php
+++ advertiser/dashboard.php
@@ -251,26 +251,26 @@ if($results123->num_rows == 0)
 
 	<script>
 
-	window.onload = function(){
-
-		// Line chart from swirlData for dashReport
-		var ctx = document.getElementById("dashReport").getContext("2d");
-		window.myLine = new Chart(ctx).Line(swirlData, {
-			responsive: true,
-			scaleShowVerticalLines: false,
-			scaleBeginAtZero : true,
-			multiTooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
-		});
-
-		// Pie Chart from doughutData
-		var doctx = document.getElementById("chart-area3").getContext("2d");
-		window.myDoughnut = new Chart(doctx).Pie(doughnutData, {responsive : true});
-
-		// Dougnut Chart from doughnutData
-		var doctx = document.getElementById("chart-area4").getContext("2d");
-		window.myDoughnut = new Chart(doctx).Doughnut(doughnutData, {responsive : true});
-
-	}
+	// window.onload = function(){
+	//
+		// // Line chart from swirlData for dashReport
+		// var ctx = document.getElementById("dashReport").getContext("2d");
+		// window.myLine = new Chart(ctx).Line(swirlData, {
+		// 	responsive: true,
+		// 	scaleShowVerticalLines: false,
+		// 	scaleBeginAtZero : true,
+		// 	multiTooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value %>",
+		// });
+		//
+		// // Pie Chart from doughutData
+		// var doctx = document.getElementById("chart-area3").getContext("2d");
+		// window.myDoughnut = new Chart(doctx).Pie(doughnutData, {responsive : true});
+		//
+		// // Dougnut Chart from doughnutData
+		// var doctx = document.getElementById("chart-area4").getContext("2d");
+		// window.myDoughnut = new Chart(doctx).Doughnut(doughnutData, {responsive : true});
+		//
+	// }
 	</script>
 </body>
 </html>
